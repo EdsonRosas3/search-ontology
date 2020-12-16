f95be217376436ca5002822e08b3fe16295f66a6
EdsonRosas3 <edsonrosas321@gmail.com>
2020-12-16 00:23:42 -0400

    estilos del buscador implementado

---
+++ src/main/java/com/buscador/buscadorontology/BuscadorOntologyApplication.java
@@ -0,0 +1,13 @@
+package com.buscador.buscadorontology;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuscadorOntologyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuscadorOntologyApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/buscador/buscadorontology/controller/buscadorController.java
@@ -0,0 +1,26 @@
+package com.buscador.buscadorontology.controller;
+
+import com.buscador.buscadorontology.model.Frase;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class buscadorController {
+
+    @GetMapping("/")
+    public String buscador(Model model){
+        model.addAttribute("frase", new Frase());
+        return "form";
+    }
+    @PostMapping("/resultados")
+    public ModelAndView resultados(@ModelAttribute("frase") Frase frase){
+        ModelAndView mav = new ModelAndView("resultados");
+        mav.addObject("frase", frase);
+        return mav;
+    }
+}
